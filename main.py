a6afdf90a49d39fa77c25a4db93d95fabb128b12
Eric Dong <145395544+edong36@users.noreply.github.com>
2024-11-12 01:55:12 +0000

    Add team number to home page

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 st.set_page_config(page_title="Home")
 
 st.title('Flood Prediction ML Project')
+st.header('Team 88')
 st.write('Grace Smith, Mansi Gupta, Ryan Otsuka, Eric Dong, Athena Hung')
 st.write('')
 st.write('Use the links in the sidebar to view different stages of our project!')
